8ad40f70b79cef81b8194e75b49d9bdf93230531
Noura Saad <noura.saad1419@gmail.com>
2024-01-25 13:35:25 +0300

    pp

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-  //hhhh
+
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  //hhh
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
