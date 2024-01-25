c3123d5974da59484383c32c06236d9a9fae51a9
Noura Saad <noura.saad1419@gmail.com>
2024-01-25 13:19:44 +0300

    pp

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,20 @@
 // hi from afnan
 
 
+  // This widget is the root of your application.
+  //hhh
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
 
 
