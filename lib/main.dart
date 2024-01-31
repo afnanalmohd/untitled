a2f979622b2b5307d22294be1b8f51c1e40f33e7
NouraSaad19 <96193859+NouraSaad19@users.noreply.github.com>
2024-01-31 11:08:05 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,5 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-// hi from afnan
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      // theme: ThemeData(
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      //   useMaterial3: true,
-      // ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
 }
